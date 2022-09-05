fbf7fbfe3cd9415117532c0e2e76d7c4f0465182
@eko.prasetiyo <eko.prasetiyo@sent.tv>
2022-09-05 15:41:46 +0700

    Initial Commit

---
+++ customCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customCollection
+//
+//  Created by Eko Prasetiyo on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
